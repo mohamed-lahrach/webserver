e2857ada32907fee7ffbd8c5d7ce65da7fbffbf1
itselcid <its.elcid01@gmail.com>
2025-07-27 20:39:19 +0100

    Enhance request handling: implement request parsing, add output for method, path, and version; update header file with accessors for request attributes

--- request/request.cpp
+++ request/request.cpp
@@ -1,4 +1,7 @@
 #include "request.hpp"
+#include <sstream>
+#include <iostream>
+
 
 // Constructor
 Request::Request()
@@ -12,6 +15,14 @@ Request::~Request()
 bool Request::handle_request(int client_fd, const char *request_data)
 {
 	(void)client_fd;
-	(void)request_data;
+	
+	std::string request_str = request_data;
+	std::istringstream iss(request_str);
+	iss >> method >> path >> version;
+	std::cout << "Method: " << method << std::endl;
+	std::cout << "Path: " << path << std::endl;
+	std::cout << "Version: " << version << std::endl;
+	
+
 	return (true);
 }
--- request/request.hpp
+++ request/request.hpp
@@ -6,12 +6,19 @@
 class Request
 {
   private:
+  std::string method;
+  std::string path;
+  std::string version;
   public:
 	// Constructor
 	Request();
 
 	// Destructor
 	~Request();
+	const std::string& get_method() const { return method; }
+	const std::string& get_path() const { return path; }
+	const std::string& get_version() const { return version; }
+
 	bool handle_request(int client_fd, const char *request_data);
 };
 
