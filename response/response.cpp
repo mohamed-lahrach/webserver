511223c42bf061eb2dd587e49aea31023f1e916f
itselcid <its.elcid01@gmail.com>
2025-07-30 17:07:45 +0100

    Fix hostname in main.cpp and enhance response handling in response.cpp and response.hpp

--- response/response.cpp
+++ response/response.cpp
@@ -1,29 +1,68 @@
 #include "response.hpp"
+#include <sstream>
+#include <iostream>
 
 // Constructor
-Response::Response()
+Response::Response() : status_code(200), content("Welcome to My Web Server!")
 {
+	// Set default headers
+	set_header("Content-Type", "text/html");
+	set_header("Connection", "close");
 }
 
 // Destructor
 Response::~Response()
 {
 }
+
+// Setter methods
+void Response::set_code(int code)
+{
+	status_code = code;
+}
+
+void Response::set_content(const std::string& body_content)
+{
+	content = body_content;
+}
+
+void Response::set_header(const std::string& key, const std::string& value)
+{
+	headers[key] = value;
+}
+
 void Response::handle_response(int client_fd)
 {
 	std::cout << "=== RESPONSE: Building and sending response ===" << std::endl;
+	
+	std::ostringstream status_stream;
+	status_stream << status_code;
+	std::string code_str = status_stream.str();
+	std::string reason_phrase = "OK";  
+	std::string status_line = "HTTP/1.0 " + code_str + " " + reason_phrase + "\r\n";
+	std::string headers_line;
+	std::map<std::string ,std::string>::iterator ite=headers.begin();
 
-	// Simple HTTP response
-	const char *response = "HTTP/1.1 200 OK\r\n"
-							"Content-Type: text/html\r\n"
-							"Content-Length: 25\r\n"
-							"Connection: close\r\n"
-							"\r\n"
-							"Welcome to My Web Server!";
+	while (ite!=headers.end())
+	{
+		headers_line+=ite->first + ": " + ite->second + "\r\n";
+		ite++;
+	}
+	
+	std::ostringstream content_length_stream;
+	content_length_stream << content.length();
+	headers_line += "Content-Length: " + content_length_stream.str() + "\r\n";
+	headers_line += "\r\n";  
 
-	// Send response to client
+	std::string full_response = status_line + headers_line + content;
+
+	std::cout << "status line: " << status_line;
+	std::cout << "headers: " << headers_line;
+	std::cout << "content: " << content << std::endl;
+	std::cout << "=== Complete Response ===\n" << full_response << "========================" << std::endl;
+	
 	std::cout << "Sending response to client " << client_fd << std::endl;
-	ssize_t bytes_sent = send(client_fd, response, strlen(response), 0);
+	ssize_t bytes_sent = send(client_fd, full_response.c_str(), full_response.length(), 0);
 
 	if (bytes_sent > 0)
 	{
--- response/response.hpp
+++ response/response.hpp
@@ -13,22 +13,17 @@ class Response
   std::string content;
   std::map<std::string, std::string> headers;
   public:
+	// Constructor
 	Response();
 
-	void set_code(int code){
-		status_code=code;
-	};
-	void set_content(const std::string& body_content)
-	{
-		content = body_content;
-	}
-	void set_header(const std::string& key,const std::string& value)
-	{
-		headers[key]=value;
-	}
-
-
+	// Destructor
 	~Response();
+	
+	// Setter methods
+	void set_code(int code);
+	void set_content(const std::string& body_content);
+	void set_header(const std::string& key, const std::string& value);
+	
 	void handle_response(int client_fd);
 };
 
